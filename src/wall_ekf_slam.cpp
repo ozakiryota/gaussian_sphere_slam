85e9006fc270d5f46bf6f7da89e5aebeeeaf8f97
ozakiryota <kb22056@gmail.com>
2019-05-16 20:50:25 +0900

    updated

--- src/wall_ekf_slam.cpp
+++ src/wall_ekf_slam.cpp
@@ -249,6 +249,8 @@ void WallEKFSLAM::PredictionOdom(nav_msgs::Odometry odom, double dt)
 	jF.block(3, 3, 3, 3) = Eigen::Matrix3d::Identity();
 	jF.block(3, size_robot_state, 3, num_wall*size_wall_state) = Eigen::MatrixXd::Zero(3, num_wall*size_wall_state);
 	/*jF-wall_xyz*/
+	jF.block(size_robot_state, 0, 3, 3) = Eigen::Matrix3d::Zero();
+	jF.block(size_robot_state, 3, 3, 3) = Eigen::Matrix3d::Zero();
 	jF.block(size_robot_state, size_robot_state, num_wall*size_wall_state, num_wall*size_wall_state) = Eigen::MatrixXd::Identity(num_wall*size_wall_state, num_wall*size_wall_state);
 
 	/*Q*/
@@ -449,7 +451,7 @@ void WallEKFSLAM::Publication(void)
 	}
 	for(size_t i=0;i<X.size();i++){	//test
 		if(std::isnan(X(i))){
-			std::cout << "X(i) is NAN" << std::endl;
+			std::cout << "NAN error" << std::endl;
 			std::cout << "X(" << i << ") = " << X(i) << std::endl;
 			exit(1);
 		}
