1b4e13827b8b5adc62be15e9d958bd9bc7b93a08
ozakiryota <kb22056@gmail.com>
2019-08-27 15:18:50 +0900

    updated

--- src/d_gaussian_sphere.cpp
+++ src/d_gaussian_sphere.cpp
@@ -302,6 +302,7 @@ void DGaussianSphere::ClusterDGauss(void)
 	}
 
 	std::cout << "clustering time [s] = " << ros::Time::now().toSec() - time_start << std::endl;
+	std::cout << "d_gaussian_sphere_clustered->points.size() : " << d_gaussian_sphere_clustered->points.size() << " -> " << decimated_size << std::endl;
 }
 
 void DGaussianSphere::Visualization(void)
